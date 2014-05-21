a53b2f7d7b1a8039075e0eb00e471678876c839a
Piotr-Kwiatkowski <piotrkwiatkowski1@gmail.com>
2014-05-21 23:30:00 +0200

    TODO update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 // TODO (Magda#1#05/18/14):
-//1. losowe wybieranie imion w konstruktorze Bunny ...
-//2. konstruktor Colony
-//3. cycle() nie ma inicjalizowac!
-//4. ...
+// 1. losowe wybieranie imion w konstruktorze Bunny
+// 2. konstruktor Colony
+// 3. cycle() nie ma inicjalizowac!
+// 4. zabezpieczenie programu przed wywaleniem w przypadku braku pliku "Imiona.csv"
+// 5. optymalizacja programu, zeby wywolywal tylko jedna metode (klasy Colony?) w main'ie
 
 #include <iostream>
 #include "Bunny.h"
